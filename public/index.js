26853c4fd3348f381a165956b966ac226f161717
Josh Miccolo <jmiccolo89@gmail.com>
2020-11-29 15:30:11 -0500

    Fix mobile text size

--- public/index.js
+++ public/index.js
@@ -4,10 +4,7 @@ setInterval(function(){
         setTimeout(async function(){
             let affirm = await axios["get"]("/getAffirmation").then(res=> res.data);
             text.innerText = affirm;
-            let r = Math.floor(Math.random() * 255)
-            let g = Math.floor(Math.random() * 255)
-            let b = Math.floor(Math.random() * 255)
-            let color = `rgb(${r}, ${g}, ${b})`
+            let color = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
             document.querySelector("body").style.backgroundColor = color;
             text.classList.remove("hide");
         },1500);
